e2a37468faeab09187c8fdb0443f5dc748075736
DavidRodriguezSoaresCUI <fireblaze904@gmail.com>
2020-05-05 19:20:40 +0200

    user-service: first alpha release

---
+++ user-service/src/main/java/api/rest/UserRestService.java
@@ -0,0 +1,79 @@
+package api.rest;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import api.msg.userProducer;
+import domain.model.User;
+import domain.service.userService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+
+@ApplicationScoped
+@Path("/user")
+@Api(value = "user", authorizations = {
+	      @Authorization(value="sampleoauth", scopes = {})
+	    })
+public class UserRestService {
+
+	@Inject
+	private userService userService;
+	@Inject
+	private userProducer userProducer;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Get all the Users",
+    notes = "Users are specialized and thus might contain more fields than the one of the base class.")
+	public List<User> getAll() {
+		return userService.getAll();
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Get a specifc User",
+    notes = "Users are specialized and thus might contain more fields than the one of the base class.")
+	public User get(@PathParam("id") Long userId) {
+		return userService.get(userId);
+	}
+	
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Update a given User",
+    notes = "Users are specialized and thus might contain more fields than the one of the base class.")
+	public void upadte(User user) {
+		userService.update(user);
+		userProducer.send(user);
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Create a new User",
+    notes = "Users are specialized and thus might contain more fields than the one of the base class.")
+	public void create(User user) {
+		userService.create(user);
+		userProducer.send(user);
+	}
+
+	
+	@POST
+	@Path("propagateAllUsers")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Propagate all Users to the bus to sync up downstream services",
+    notes = "Users are specialized and thus might contain more fields than the one of the base class.")
+	public void propagateAllUsers() {
+		userProducer.sendAllUsers();
+	}
+}
---
+++ user-service/src/main/java/domain/service/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package domain.service;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+import domain.model.User;
+
+@ApplicationScoped
+public class UserServiceImpl implements UserService {
+
+	@PersistenceContext(unitName = "UserPU")
+	private EntityManager em;
+
+	public UserServiceImpl() {
+	}
+
+	public UserServiceImpl(EntityManager em) {
+		this();
+		this.em = em;
+	}
+
+	@Override
+	public List<User> getAll() {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<User> criteria = builder.createQuery(User.class);
+		criteria.from(User.class);
+		return em.createQuery(criteria).getResultList();
+	}
+
+	@Override
+	public void update(User user) {
+		User i = em.find(User.class, user.getId());
+		if (i == null) {
+			throw new IllegalArgumentException("User does not exist : " + user.getId());
+		}
+		em.merge(user);
+	}
+
+	@Override
+	public User get(Long userId) {
+		return em.find(User.class, userId);
+	}
+
+	@Override
+	public void create(User user) {
+		if ( /*user.getId() != null or*/ get(user.getId())!=null ) {
+			throw new IllegalArgumentException("User already exists : " + user.getId());
+		}
+		em.persist(user);
+	}
+}
