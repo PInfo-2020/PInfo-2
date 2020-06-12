1703241dc1b3e560f17dad659513479aeec426dc
eikowww <Julien.Mehmeti@etu.unige.ch>
2020-06-12 11:50:46 +0200

    add interface

--- .history/recipe-service-make_20200612110826.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#add ' -rf :recipe-service' to skip already-compiled services to save time (not on the first time !)
-#mvn clean install -Dmaven.test.skip=true
-# Uncomment when maven clean install compiles without error
-mvn install -Ppackage-docker-image -Dmaven.test.skip=true -rf :recipe-service
-docker build -t "web-ui:latest" .
