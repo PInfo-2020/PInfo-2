37a307b1ee790daf1b282bdc594491ebae8708a3
DavidRodriguezSoaresCUI <fireblaze904@gmail.com>
2020-06-12 13:43:51 +0200

    Now recipe/user services should retain database between launches, also any new microservice created from microservice-builder

--- microservice-builder/__template__-service/__template__-service-start.sh
+++ microservice-builder/__template__-service/__template__-service-start.sh
@@ -4,7 +4,7 @@
 v_list=$(docker ps -aq)
 docker stop $v_list 2>/dev/null
 docker rm $v_list 2>/dev/null
-docker volume rm docker-compose_pgdata-__db_name__ 2>/dev/null
+#docker volume rm docker-compose_pgdata-__db_name__ 2>/dev/null
 
 # Launching service
 
--- user-service-start.sh
+++ user-service-start.sh
@@ -5,7 +5,7 @@ v_list=$(docker ps -aq)
 docker stop $v_list 2>/dev/null
 docker rm $v_list 2>/dev/null
 #docker volume rm docker-compose_pgdata-inst 2>/dev/null
-docker volume rm docker-compose_pgdata-usr 2>/dev/null
+#docker volume rm docker-compose_pgdata-usr 2>/dev/null
 
 # Launching service
 
