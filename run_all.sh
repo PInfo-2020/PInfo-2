98041b3f0b13932cc4f0b19a8bac662633d73ee0
DavidRodriguezSoaresCUI <fireblaze904@gmail.com>
2020-06-18 21:41:38 +0200

    Run all script and docker-compose file

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run all services with Docker Compose
-docker-compose -f docker-compose/docker-compose-microservices_user.yml -f docker-compose/docker-compose-microservices_recipe.yml up
+docker-compose -f docker-compose/docker-compose-all.yml up
